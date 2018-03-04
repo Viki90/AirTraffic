dc45a3265582078e364a70d52479ad76f0903e34
Viki <viki.verebes90@gmail.com>
2018-03-04 15:43:15 +0100

    Added function for get list of flights

--- js/main.js
+++ js/main.js
@@ -34,30 +34,57 @@ function getDataForGeolocation() {
 
     long = crd.longitude;
     lat = crd.latitude;
-    $.ajax({
-        url: "http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=" + lat + "&lng=" + long,
-        dataType: 'jsonp',
-        error: function() {
-            $('#message').html("<p>We couldn't retrive your data for your longitude and latitude. Please true again.</p>");
-        },
-        success: function(data){
-            $('#message').html("<p>It is success.</p>");
-         
-console.log(data)
-            for (var obj in data.acList){
-                for(var innerObj in obj){
-                    if(innerObj.Year == "2016"){
-                        console.log("innerObj");
-                        console.log(innerObj.Year);
+        $.ajax({
+            url: "http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=" + lat + "&lng=" + long,
+            dataType: 'jsonp',
+            error: function() {
+                $('#message').html("<p>We couldn't retrive your data for your longitude and latitude. Please true again.</p>");
+            },
+            success: function(data){
+                $('#message').html("<p>It is success.</p>");
+             
+                //console.log(data)
+                var listToShow = [];
+                var listAlt = [];
+                for(var i = 0; i < data.acList.length; i++){
+                    //console.log(data.acList[i].length)
+                    //console.log("Alt");
+                    //console.log(data.acList[i].Alt);
+                    //console.log("Alt");
+                    //console.log("Year");
+                    //console.log(data.acList[i].Year);
+                    if(i < 9){
+                        listToShow.push(data.acList[i]);
+                    }else{
+                        listToShow.sort(function(a, b){
+                            return b.Alt - a.Alt;
+                        });
+                       // for(var i = 0; i < listToShow.length; i++){
+                           
+                            listOfFlights(listToShow);
+                        //}
+
+                        break;
                     }
+
                 }
             }
-        }
-
+        });
     });
-});
-};
+}
 
 function errorCallback(err) {
   $('#message').html("<p>We couldn't retrive your data for your longitude and latitude. Please true again.</p>");
 };
+
+function listOfFlights(listToShow){
+    $('#list').append('<h2>Flights:</h2>');
+    for (var i = 0; i < listToShow.length; i++) {
+         $('#list').append('<ul><li>Flight code: '+ listToShow[i].Id +'</li>' + 
+                            '<li>Altitude: ' + listToShow[i].Alt + '</li>' +
+                            '<li>Destination: ' + listToShow[i].Cou + '</li></ul>');
+    }
+
+    
+    
+}
